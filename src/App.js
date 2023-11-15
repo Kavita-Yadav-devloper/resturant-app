361ee1930ecc071c01f1697c164f983a44eae3e5
kavita <ykittu786@gmail.com>
2023-11-15 17:50:02 +0530

    resturant react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+ import Resturant from './component/Basics/Resturant'
+//import UseState from './component/Hooks/UseState'
+// import UseEffect from './component/Hooks/UseEffect'
+import UseReducer from './component/Hooks/UseReducer'
+const App = () => {
+  return (
+   <>
+   {/* <UseState /> */}
+  <Resturant/> 
+  {/* <UseEffect /> */}
+  {/* <UseReducer /> */}
+   </>
+  )
+}
+
+export default App
